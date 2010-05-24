b8c345a7649b34b93de081fd9330af622052b5cd
Ralf Bensmann <ralf@art-of-coding.eu>
2010-05-24 21:36:42 +0200

    Added comment

--- griffon-app/bindings/com/westaflex/wac/RaumdatenBindings.groovy
+++ griffon-app/bindings/com/westaflex/wac/RaumdatenBindings.groovy
@@ -4,4 +4,5 @@ package com.westaflex.wac
 // Binding of comboboxes is done in RaumdatenView!
 bind(source: model.map.raum, sourceProperty: "hohe",         target: raumHohe,         targetProperty: "text")
 bind(source: model.map.raum, sourceProperty: "zuluftfaktor", target: raumZuluftfaktor, targetProperty: "text")
+// Raum hinzufügen
 raumHinzufugen.actionPerformed = controller.raumHinzufugen
