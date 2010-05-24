7ba7494e0cc799ff3041873edd80b14c84924434
Ralf Bensmann <ralf@art-of-coding.eu>
2010-05-24 20:22:56 +0200

    Added bindings for default values

--- griffon-app/bindings/com/westaflex/wac/RaumdatenBindings.groovy
+++ griffon-app/bindings/com/westaflex/wac/RaumdatenBindings.groovy
@@ -2,4 +2,6 @@ package com.westaflex.wac
 
 // Raumdaten - Raum-Eingabe
 // Binding of comboboxes is done in RaumdatenView!
+bind(source: model.map.raum, sourceProperty: "hohe",         target: raumHohe,         targetProperty: "text")
+bind(source: model.map.raum, sourceProperty: "zuluftfaktor", target: raumZuluftfaktor, targetProperty: "text")
 raumHinzufugen.actionPerformed = controller.raumHinzufugen
