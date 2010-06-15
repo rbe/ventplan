961a1ab3af130e9e5f0ff340883086dff0c2bf31
Ralf Bensmann <ralf@art-of-coding.eu>
2010-06-15 18:55:59 +0200

    Removed mutual bindings and set converter for TextFields

--- griffon-app/bindings/com/westaflex/wac/GebaudedatenBindings.groovy
+++ griffon-app/bindings/com/westaflex/wac/GebaudedatenBindings.groovy
@@ -1,5 +1,7 @@
 package com.westaflex.wac
 
+import com.bensmann.griffon.GriffonHelper as GH
+
 // Gebäudedaten - Gebäudetyp
 bind(source: model.map.gebaude.typ, sourceProperty: "MFH",        target: gebaudeTypMFH,        targetProperty: "selected", mutual: true)
 bind(source: model.map.gebaude.typ, sourceProperty: "EFH",        target: gebaudeTypEFH,        targetProperty: "selected", mutual: true)
@@ -11,13 +13,13 @@ bind(source: model.map.gebaude.lage, sourceProperty: "windstark",   target: geba
 bind(source: model.map.gebaude.warmeschutz, sourceProperty: "hoch",    target: gebaudeWarmeschutzHoch,    targetProperty: "selected", mutual: true)
 bind(source: model.map.gebaude.warmeschutz, sourceProperty: "niedrig", target: gebaudeWarmeschutzNiedrig, targetProperty: "selected", mutual: true)
 // Gebäudedaten - Geometrie
-bind(source: model.map.gebaude.geometrie, sourceProperty: "wohnflache",        target: gebaudeGeometrieWohnflache,        targetProperty: "text", mutual: true)
-bind(source: model.map.gebaude.geometrie, sourceProperty: "raumhohe",          target: gebaudeGeometrieMittlereRaumhohe,  targetProperty: "text", mutual: true)
-bind(source: model.map.gebaude.geometrie, sourceProperty: "luftvolumen",       target: gebaudeGeometrieLuftvolumen,       targetProperty: "text", mutual: true)
-bind(source: model.map.gebaude.geometrie, sourceProperty: "gelufteteFlache",   target: gebaudeGeometrieGelufteteFlache,   targetProperty: "text", mutual: true)
-bind(source: model.map.gebaude.geometrie, sourceProperty: "geluftetesVolumen", target: gebaudeGeometrieGeluftetesVolumen, targetProperty: "text", mutual: true)
+bind(source: model.map.gebaude.geometrie, sourceProperty: "wohnflache",        target: gebaudeGeometrieWohnflache,        targetProperty: "text", converter: GH.toString2Converter)
+bind(source: model.map.gebaude.geometrie, sourceProperty: "raumhohe",          target: gebaudeGeometrieMittlereRaumhohe,  targetProperty: "text", converter: GH.toString2Converter)
+bind(source: model.map.gebaude.geometrie, sourceProperty: "luftvolumen",       target: gebaudeGeometrieLuftvolumen,       targetProperty: "text", converter: GH.toString2Converter)
+bind(source: model.map.gebaude.geometrie, sourceProperty: "gelufteteFlache",   target: gebaudeGeometrieGelufteteFlache,   targetProperty: "text", converter: GH.toString2Converter)
+bind(source: model.map.gebaude.geometrie, sourceProperty: "geluftetesVolumen", target: gebaudeGeometrieGeluftetesVolumen, targetProperty: "text", converter: GH.toString2Converter)
 [gebaudeGeometrieWohnflache, gebaudeGeometrieMittlereRaumhohe, gebaudeGeometrieGelufteteFlache].each {
-	it.caretUpdate = controller.berechneGeometrie
+	it.focusLost = controller.berechneGeometrie
 }
 // Luftdichtheit der Gebäudehülle
 bind(source: model.map.gebaude.luftdichtheit, sourceProperty: "kategorieA", target: gebaudeLuftdichtheitKategorieA, targetProperty: "selected", mutual: true)
@@ -28,19 +30,23 @@ bind(source: model.map.gebaude.luftdichtheit, sourceProperty: "messwerte",  targ
 bind(source: gebaudeLuftdichtheitMesswerte, sourceProperty: "selected", target: gebaudeLuftdichtheitDruckdifferenz, targetProperty: "enabled")
 bind(source: gebaudeLuftdichtheitMesswerte, sourceProperty: "selected", target: gebaudeLuftdichtheitLuftwechsel,    targetProperty: "enabled")
 bind(source: gebaudeLuftdichtheitMesswerte, sourceProperty: "selected", target: gebaudeLuftdichtheitDruckexponent,  targetProperty: "enabled")
-bind(source: model.map.gebaude.luftdichtheit, sourceProperty: "druckdifferenz", target: gebaudeLuftdichtheitDruckdifferenz, targetProperty: "text", mutual: true)
-bind(source: model.map.gebaude.luftdichtheit, sourceProperty: "luftwechsel",    target: gebaudeLuftdichtheitLuftwechsel,    targetProperty: "text", mutual: true)
-bind(source: model.map.gebaude.luftdichtheit, sourceProperty: "druckexponent",  target: gebaudeLuftdichtheitDruckexponent,  targetProperty: "text", mutual: true)
+bind(source: model.map.gebaude.luftdichtheit, sourceProperty: "druckdifferenz", target: gebaudeLuftdichtheitDruckdifferenz, targetProperty: "text", converter: GH.toString2Converter)
+bind(source: model.map.gebaude.luftdichtheit, sourceProperty: "luftwechsel",    target: gebaudeLuftdichtheitLuftwechsel,    targetProperty: "text", converter: GH.toString2Converter)
+bind(source: model.map.gebaude.luftdichtheit, sourceProperty: "druckexponent",  target: gebaudeLuftdichtheitDruckexponent,  targetProperty: "text", converter: GH.toString3Converter)
+[gebaudeLuftdichtheitDruckdifferenz, gebaudeLuftdichtheitLuftwechsel, gebaudeLuftdichtheitDruckexponent].each {
+	it.focusLost = controller.speichereLuftdichtheit
+}
 // Luftdichtheit der Gebäudehülle - Kategorien
 gebaudeLuftdichtheitKategorieA.actionPerformed = controller.luftdichtheitKategorieA
 gebaudeLuftdichtheitKategorieB.actionPerformed = controller.luftdichtheitKategorieB
 gebaudeLuftdichtheitKategorieC.actionPerformed = controller.luftdichtheitKategorieC
 // Besondere Anforderungen
-bind(source: model.map.gebaude, sourceProperty: "faktorBesondereAnforderungen", target: faktorBesondereAnforderungen, targetProperty: "text", mutual: true)
+bind(source: model.map.gebaude, sourceProperty: "faktorBesondereAnforderungen", target: faktorBesondereAnforderungen, targetProperty: "text", converter: GH.toString2Converter)
+faktorBesondereAnforderungen.focusLost = controller.speichereFaktorBesondereAnforderungen
 // Geplante Belegung
 bind(source: model.map.gebaude.geplanteBelegung, sourceProperty: "personenanzahl",        target: gebaudeGeplantePersonenanzahl,        targetProperty: "value", mutual: true)
 bind(source: model.map.gebaude.geplanteBelegung, sourceProperty: "aussenluftVsProPerson", target: gebaudeGeplanteAussenluftVsProPerson, targetProperty: "value", mutual: true)
-bind(source: model.map.gebaude.geplanteBelegung, sourceProperty: "mindestaussenluftrate", target: gebaudeGeplanteMindestaussenluftrate, targetProperty: "text", mutual: true)
+bind(source: model.map.gebaude.geplanteBelegung, sourceProperty: "mindestaussenluftrate", target: gebaudeGeplanteMindestaussenluftrate, targetProperty: "text",  converter: GH.toString2Converter)
 [gebaudeGeplantePersonenanzahl, gebaudeGeplanteAussenluftVsProPerson].each {
 	it.stateChanged = controller.berechneMindestaussenluftrate
 }
