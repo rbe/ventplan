628083bef687a1d24cea6b19e13aebb18d7c3c39
Ralf Bensmann <ralf@art-of-coding.eu>
2010-06-15 18:52:19 +0200

    Added events: these code gets executed on "publishEvent(name)"-calls (used mainly by controllers)

---
+++ griffon-app/events/ProjektEvents.groovy
@@ -0,0 +1,53 @@
+//package com.westaflex.wac
+
+/**
+ * 
+ */
+@griffon.util.EventPublisher
+class ProjektEvents {
+	
+	def model
+	def wacCalculationService
+	
+	/**
+	 * 
+	 */
+	def onGeometrieEingegeben = {
+		javax.swing.SwingUtilities.invokeLater {
+			println "processing event 'GeometrieEingegeben'"
+			wacCalculationService.geometrie(model.map)
+			wacCalculationService.aussenluftVs(model.map)
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	def onAussenluftVsBerechnen = {
+		javax.swing.SwingUtilities.invokeLater {
+			println "processing event 'AussenluftVsBerechnen'"
+			wacCalculationService.aussenluftVs(model.map)
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	def onRaumHinzugefugt = {
+		javax.swing.SwingUtilities.invokeLater {
+			println "processing event 'RaumHinzugefugt'"
+			wacCalculationService.geometrieAusRaumdaten(model.map)
+			wacCalculationService.aussenluftVs(model.map)
+			model.syncRaumTableModels()
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	def onRaumEntfernt = {
+		println "processing event 'RaumEntfernt', delegating to 'RaumHinzugefugt'"
+		onRaumHinzugefugt()
+	}
+	
+}
