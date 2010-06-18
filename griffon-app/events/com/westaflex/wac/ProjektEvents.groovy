45249619e44a6870d45323fe42e3a882cdc552f8
Ralf Bensmann <ralf@art-of-coding.eu>
2010-06-18 13:19:04 +0200

    Refactored ProjektEvents into a package

--- griffon-app/events/com/westaflex/wac/ProjektEvents.groovy
+++ griffon-app/events/com/westaflex/wac/ProjektEvents.groovy
@@ -1,4 +1,12 @@
-//package com.westaflex.wac
+/**
+ * /Users/rbe/project/westaflex/WestaWAC2/griffon-app/events/ProjektEvents.groovy
+ * 
+ * Copyright (C) 2010 Informationssysteme Ralf Bensmann.
+ * Alle Rechte vorbehalten. Nutzungslizenz siehe http://www.bensmann.com/license_de.html
+ * All Rights Reserved. Use is subject to license terms, see http://www.bensmann.com/license_en.html
+ * 
+ */
+package com.westaflex.wac
 
 /**
  * 
@@ -49,6 +57,28 @@ class ProjektEvents {
 	def onRaumEntfernt = {
 		println "processing event 'RaumEntfernt', delegating to 'RaumHinzugefugt'"
 		onRaumHinzugefugt()
+	}
+	
+	/**
+	 * 
+	 */
+	def onRaumZuAbluftventileLuftmengeBerechnen = { raumIndex ->
+		println "onRaumZuAbluftventileLuftmengeBerechnen: Berechne Luftmenge für Raum Nr. ${raumIndex}"
+		javax.swing.SwingUtilities.invokeLater {
+			wacCalculationService.raumLuftmengeBerechnen(map, raumIndex)
+			model.syncRaumTableModels()
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	def onRaumUberstromelementeLuftmengeBerechnen = { raumIndex ->
+		println "onRaumUberstromelementeLuftmengeBerechnen: Berechne Luftmenge für Raum Nr. ${raumIndex}"
+		javax.swing.SwingUtilities.invokeLater {
+			wacCalculationService.raumLuftmengeBerechnen(map, raumIndex)
+			model.syncRaumTableModels()
+		}
 	}
 	
 }
