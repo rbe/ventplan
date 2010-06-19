374c8064e706460ed446fb242442e664f845b3e0
Ralf Bensmann <ralf@art-of-coding.eu>
2010-06-19 12:23:37 +0200

    Added copyright

--- griffon-app/views/Wac2MenuBar.groovy
+++ griffon-app/views/Wac2MenuBar.groovy
@@ -1,3 +1,11 @@
+/**
+ * /Users/rbe/project/westaflex/WestaWAC2/griffon-app/views/Wac2MenuBar.groovy
+ * 
+ * Copyright (C) 2010 Informationssysteme Ralf Bensmann.
+ * Alle Rechte vorbehalten. Nutzungslizenz siehe http://www.bensmann.com/license_de.html
+ * All Rights Reserved. Use is subject to license terms, see http://www.bensmann.com/license_en.html
+ * 
+ */
 import static griffon.util.GriffonApplicationUtils.*
 
 menuBar = menuBar {
