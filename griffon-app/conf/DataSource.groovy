bb616dd45f6eb10ecd99fcd3faa0ff369dc9ad06
Ralf Bensmann <ralf@art-of-coding.eu>
2010-06-19 12:18:19 +0200

    Setup WestaWAC MySQL datasource

--- griffon-app/conf/DataSource.groovy
+++ griffon-app/conf/DataSource.groovy
@@ -1,6 +1,6 @@
 dataSource {
-	url = "jdbc:hsqldb:file:devDB;shutdown=true"
-	driverClassname = "org.hsqldb.jdbcDriver"
-	username = "sa"
+	url = "jdbc:mysql://localhost:3306/Westa" //"jdbc:hsqldb:file:devDB;shutdown=true"
+	driverClassName = "com.mysql.jdbc.Driver" //"org.hsqldb.jdbcDriver"
+	username = "westa"
 	password = ""
 }
