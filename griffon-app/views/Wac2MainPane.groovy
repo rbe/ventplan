2794012e16a6d4355b7c18bc2cba2e6a8db4795c
Ralf Bensmann <ralf@art-of-coding.eu>
2010-06-23 13:31:10 +0200

    Added copyright

--- griffon-app/views/Wac2MainPane.groovy
+++ griffon-app/views/Wac2MainPane.groovy
@@ -1,3 +1,12 @@
+/**
+ * /Users/rbe/project/westaflex/WestaWAC2/griffon-app/views/Wac2MainPane.groovy
+ * 
+ * Copyright (C) 2010 Informationssysteme Ralf Bensmann.
+ * Alle Rechte vorbehalten. Nutzungslizenz siehe http://www.bensmann.com/license_de.html
+ * All Rights Reserved. Use is subject to license terms, see http://www.bensmann.com/license_en.html
+ * 
+ */
+
 // A tabbed pane holding all open projects
 jideTabbedPane(id: "projektTabGroup") {
 }
--- griffon-app/views/Wac2View.groovy
+++ griffon-app/views/Wac2View.groovy
@@ -1,3 +1,11 @@
+/**
+ * /Users/rbe/project/westaflex/WestaWAC2/griffon-app/views/Wac2View.groovy
+ * 
+ * Copyright (C) 2010 Informationssysteme Ralf Bensmann.
+ * Alle Rechte vorbehalten. Nutzungslizenz siehe http://www.bensmann.com/license_de.html
+ * All Rights Reserved. Use is subject to license terms, see http://www.bensmann.com/license_en.html
+ * 
+ */
 import java.awt.Toolkit
 import javax.swing.WindowConstants
 
