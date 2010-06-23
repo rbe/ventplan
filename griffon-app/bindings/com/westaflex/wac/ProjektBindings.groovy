0aaaff627167063ca4e200578da4e79edc6d9275
Ralf Bensmann <ralf@art-of-coding.eu>
2010-06-23 19:09:59 +0200

    Added copyright

--- griffon-app/bindings/com/westaflex/wac/ProjektBindings.groovy
+++ griffon-app/bindings/com/westaflex/wac/ProjektBindings.groovy
@@ -1,1 +1,9 @@
+/**
+ * /Users/rbe/project/westaflex/WestaWAC2/griffon-app/bindings/com/westaflex/wac/ProjektBindings.groovy
+ * 
+ * Copyright (C) 2010 Informationssysteme Ralf Bensmann.
+ * Alle Rechte vorbehalten. Nutzungslizenz siehe http://www.bensmann.com/license_de.html
+ * All Rights Reserved. Use is subject to license terms, see http://www.bensmann.com/license_en.html
+ * 
+ */
 package com.westaflex.wac
