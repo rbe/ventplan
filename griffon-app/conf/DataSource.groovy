2b7a728b1dd3a3acb57a7ce204dc038b56530aa5
Ralf Bensmann <ralf@art-of-coding.eu>
2010-06-23 13:04:30 +0200

    Added WestaWAC H2 database URL

--- griffon-app/conf/DataSource.groovy
+++ griffon-app/conf/DataSource.groovy
@@ -1,6 +1,6 @@
 dataSource {
-	url = "jdbc:mysql://localhost:3306/Westa" //"jdbc:hsqldb:file:devDB;shutdown=true"
-	driverClassName = "com.mysql.jdbc.Driver" //"org.hsqldb.jdbcDriver"
-	username = "westa"
+	url = "jdbc:h2:../sql/westawac" //"jdbc:mysql://localhost:3306/Westa" //"jdbc:hsqldb:file:devDB;shutdown=true"
+	driverClassName = "org.h2.Driver" //"com.mysql.jdbc.Driver" //"org.hsqldb.jdbcDriver"
+	username = "sa"
 	password = ""
 }
