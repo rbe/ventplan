a0eecf77d75297a5c792ab2931288a247e1e8469
Ralf Bensmann <ralf@art-of-coding.eu>
2010-06-29 12:32:27 +0200

    Added rounding mode combobox

--- griffon-app/views/com/westaflex/wac/AussenluftVsView.groovy
+++ griffon-app/views/com/westaflex/wac/AussenluftVsView.groovy
@@ -5,12 +5,12 @@ import net.miginfocom.swing.MigLayout
 // Notwendigkeit der lüftungstechnischen Maßnahme
 panel(id: "aussenluftVsNotwendigkeitLuftungstechnischeMassnahme", border: titledBorder(title: "Notwendigkeit der lüftungstechnischen Maßnahme"), layout: new MigLayout("wrap", "[left]30[right]15[left]30[left]", "rel[]rel")) {
 	label("Feuchteschutz: Gesamt-Außenluftvolumenstrom", constraints: "width 320px!")
-	label(id: "aussenluftVsGesamt", text: "  0,00", constraints: "width 50px!")
+	label(id: "aussenluftVsGesamt", text: "0,00", constraints: "width 50px!")
 	label("m³/h")
 	label(id: "aussenluftVsMassnahme", " ", foreground: java.awt.Color.RED)
 	
 	label("Luftvolumenstrom durch Infiltration")
-	label(id: "aussenluftVsInfiltration", text: "  0,00")
+	label(id: "aussenluftVsInfiltration", text: "0,00")
 	label("m³/h")
 	label("")
 }
@@ -28,16 +28,16 @@ panel(id: "aussenluftVsGesamtAussenluftVsNutzungseinheit", border: titledBorder(
 	label("")
 	
 	label("Luftvolumenstrom")
-	label(id: "gesamtAvsNeLvsFs", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtAvsNeLvsRl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtAvsNeLvsNl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtAvsNeLvsIl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsNeLvsFs", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsNeLvsRl", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsNeLvsNl", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsNeLvsIl", text: "0,00", constraints: "width 50px!"); label("m³/h")
 	
 	label("Luftwechsel")
-	label(id: "gesamtAvsNeLwFs", text: "  0,00"); label("l/h")
-	label(id: "gesamtAvsNeLwRl", text: "  0,00"); label("l/h")
-	label(id: "gesamtAvsNeLwNl", text: "  0,00"); label("l/h")
-	label(id: "gesamtAvsNeLwIl", text: "  0,00"); label("l/h")
+	label(id: "gesamtAvsNeLwFs", text: "0,00"); label("l/h")
+	label(id: "gesamtAvsNeLwRl", text: "0,00"); label("l/h")
+	label(id: "gesamtAvsNeLwNl", text: "0,00"); label("l/h")
+	label(id: "gesamtAvsNeLwIl", text: "0,00"); label("l/h")
 }
 
 // Gesamtabluftvolumenströme der Räume
@@ -53,16 +53,16 @@ panel(id: "aussenluftVsGesamtabluftVsRaume", border: titledBorder(title: "Gesamt
 	label("")
 	
 	label("Luftvolumenstrom", constraints: "width 120px!")
-	label(id: "gesamtAvsRaumLvsFs", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtAvsRaumLvsRl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtAvsRaumLvsNl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtAvsRaumLvsIl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsRaumLvsFs", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsRaumLvsRl", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsRaumLvsNl", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsRaumLvsIl", text: "0,00", constraints: "width 50px!"); label("m³/h")
 	
 	label("Luftwechsel", constraints: "width 120px!")
-	label(id: "gesamtAvsRaumLwFs", text: "  0,00"); label("l/h")
-	label(id: "gesamtAvsRaumLwRl", text: "  0,00"); label("l/h")
-	label(id: "gesamtAvsRaumLwNl", text: "  0,00"); label("l/h")
-	label(id: "gesamtAvsRaumLwIl", text: "  0,00"); label("l/h")
+	label(id: "gesamtAvsRaumLwFs", text: "0,00"); label("l/h")
+	label(id: "gesamtAvsRaumLwRl", text: "0,00"); label("l/h")
+	label(id: "gesamtAvsRaumLwNl", text: "0,00"); label("l/h")
+	label(id: "gesamtAvsRaumLwIl", text: "0,00"); label("l/h")
 }
 
 // personenbezogene Gesamt-Abluftvolumenströme
@@ -78,16 +78,16 @@ panel(id: "aussenluftVsPersonenbezogeneGesamtabluftVs", border: titledBorder(tit
 	label("")
 	
 	label("Luftvolumenstrom", constraints: "width 120px!")
-	label(id: "gesamtAvsPersonLvsFs", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtAvsPersonLvsRl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtAvsPersonLvsNl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtAvsPersonLvsIl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsPersonLvsFs", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsPersonLvsRl", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsPersonLvsNl", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtAvsPersonLvsIl", text: "0,00", constraints: "width 50px!"); label("m³/h")
 	
 	label("Luftwechsel")
-	label(id: "gesamtAvsPersonLwFs", text: "  0,00"); label("l/h")
-	label(id: "gesamtAvsPersonLwRl", text: "  0,00"); label("l/h")
-	label(id: "gesamtAvsPersonLwNl", text: "  0,00"); label("l/h")
-	label(id: "gesamtAvsPersonLwIl", text: "  0,00"); label("l/h")
+	label(id: "gesamtAvsPersonLwFs", text: "0,00"); label("l/h")
+	label(id: "gesamtAvsPersonLwRl", text: "0,00"); label("l/h")
+	label(id: "gesamtAvsPersonLwNl", text: "0,00"); label("l/h")
+	label(id: "gesamtAvsPersonLwIl", text: "0,00"); label("l/h")
 }
 
 // Gesamt-Luftvolumenstrom für lüftungstechnische Maßnahmen
@@ -101,15 +101,22 @@ panel(id: "aussenluftVsGesamtLTM", border: titledBorder(title: "Gesamt-Luftvolum
 	label("", constraints: "span")
 	
 	label("Luftvolumenstrom")
-	label(id: "gesamtLvsLtmLvsRl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtLvsLtmLvsNl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
-	label(id: "gesamtLvsLtmLvsIl", text: "  0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtLvsLtmLvsRl", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtLvsLtmLvsNl", text: "0,00", constraints: "width 50px!"); label("m³/h")
+	label(id: "gesamtLvsLtmLvsIl", text: "0,00", constraints: "width 50px!"); label("m³/h")
 	checkBox(id: "aussenluftVsGesamtLtmInfiltrationsanteil", selected: false, text: "mit Infiltrationsanteil berechnen")
 	
 	label("Luftwechsel")
-	label(id: "gesamtLvsLtmLwRl", text: "  0,00"); label("l/h")
-	label(id: "gesamtLvsLtmLwNl", text: "  0,00"); label("l/h")
-	label(id: "gesamtLvsLtmLwIl", text: "  0,00"); label("l/h", constraints: "span")
+	label(id: "gesamtLvsLtmLwRl", text: "0,00"); label("l/h")
+	label(id: "gesamtLvsLtmLwNl", text: "0,00"); label("l/h")
+	label(id: "gesamtLvsLtmLwIl", text: "0,00"); label("l/h", constraints: "span")
 }
+
+// Rounding Mode
+panel(id: "aussenluftVsRoundingModePanel", border: titledBorder(title: "Rounding Mode"), layout: new MigLayout("wrap", "[left]", "")) {
+	comboBox(id: "aussenluftVsRoundingMode", items: ["CEILING", "DOWN", "FLOOR", "HALF_DOWN", "HALF_EVEN", "HALF_UP", "UNNECESSARY", "UP"], selectedItem: "HALF_UP")
+}
+aussenluftVsRoundingMode.actionPerformed = controller.setRoundingMode
+
 // Bindings
 build(AussenluftVsBindings)
