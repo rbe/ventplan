f2fd78f3b868c15cd17150656a115d8e8515718b
Ralf Bensmann <ralf@art-of-coding.eu>
2010-07-01 11:03:45 +0200

    Improved texts showed in splash screen during startup phases

--- griffon-app/conf/Events.groovy
+++ griffon-app/conf/Events.groovy
@@ -38,86 +38,78 @@ onBootstrapEnd = { app ->
 	def stopTime = System.currentTimeMillis()
 	println "Events.onBootstrapEnd: finished in ${stopTime - startTime} ms"
 	// Set splash screen status text
-	SplashScreen.instance.showStatus("Bootstrapping done...")
+	SplashScreen.instance.showStatus("Initializing... done")
 }
 
 onStartupStart = { app ->
+	/*
 	def startTime = System.currentTimeMillis()
 	println "Events.onStartupStart: start"
-	// Load data from database
-	/*
-	withSql { sql ->
-		def tmpList = []
-		sql.eachRow("SELECT * FROM persons") {
-			tmpList << [id: it.id,
-				name: it.name,
-				lastname: it.lastname
-			]
-		}
-		edt { model.personsList.addAll(tmpList) }
-	}
-	*/
 	//
 	def stopTime = System.currentTimeMillis()
 	println "Events.onStartupStart: finished in ${stopTime - startTime} ms"
-	// Set splash screen status text
-	SplashScreen.instance.showStatus("Starting up...")
+	*/
 }
 
 /**
  * 
  */
 onStartupEnd = { app ->
+	/*
 	def startTime = System.currentTimeMillis()
 	println "Events.onStartupEnd: start"
 	//
 	def stopTime = System.currentTimeMillis()
 	println "Events.onStartupEnd: finished in ${stopTime - startTime} ms"
-	// Set splash screen status text
-	SplashScreen.instance.showStatus("Startup done...")
+	*/
 }
 
 /**
  * 
  */
 onReadyStart = { app ->
+	/*
 	def startTime = System.currentTimeMillis()
 	println "Events.onReadyStart: start"
 	//
 	def stopTime = System.currentTimeMillis()
 	println "Events.onReadyStart: finished in ${stopTime - startTime} ms"
-	// Set splash screen status text
-	SplashScreen.instance.showStatus("onReadyStart...")
+	*/
 }
 
 /**
  * 
  */
 onReadyEnd = { app ->
+	/*
 	def startTime = System.currentTimeMillis()
 	println "Events.onReadyEnd: start"
 	//
 	def stopTime = System.currentTimeMillis()
 	println "Events.onReadyEnd: finished in ${stopTime - startTime} ms"
-	// Set splash screen status text
-	SplashScreen.instance.showStatus("onReadyEnd...")
+	*/
 }
 
 /**
  * 
  */
 onShutdownStart = { app ->
+	/*
 	def startTime = System.currentTimeMillis()
 	println "Events.onShutdownStart: start"
 	//
 	def stopTime = System.currentTimeMillis()
 	println "Events.onShutdownStart: finished in ${stopTime - startTime} ms"
+	*/
 }
 
 /**
  * 
  */
 onNewInstance = { clazz, type, instance ->
+	// This is ignored when SplashScreen was disposed before,
+	// so we can use it for startup phase and ignore it when MVC groups are created
+	SplashScreen.instance.showStatus("Creating UI...")
 	println "Events.onNewInstance: clazz=${clazz} type=${type} instance=${instance}"
 }
 
@@ -125,6 +117,9 @@ onNewInstance = { clazz, type, instance ->
  * 
  */
 onCreateMVCGroup = { mvcId, model, view, controller, mvcType, instances ->
+	// This is ignored when SplashScreen was disposed before,
+	// so we can use it for startup phase and ignore it when MVC groups are created
+	SplashScreen.instance.showStatus("Creating UI... done")
 	println "Events.onCreateMVCGroup: mvcId=${mvcId}"
 }
 
