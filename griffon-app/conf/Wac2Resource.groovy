227d3dd91006838eae36a8634d5c73a302e27277
Ralf Bensmann <ralf@art-of-coding.eu>
2010-07-05 19:24:36 +0200

    ...

--- griffon-app/conf/Wac2Resource.groovy
+++ griffon-app/conf/Wac2Resource.groovy
@@ -1,7 +1,7 @@
 /**
  * /Users/rbe/project/westaflex/WestaWAC2/griffon-app/conf/Wac2Resource.groovy
  * 
- * Copyright (C) 1996-2010 Informationssysteme Ralf Bensmann.
+ * Copyright (C) 2010 Informationssysteme Ralf Bensmann.
  * Alle Rechte vorbehalten. Nutzungslizenz siehe http://www.bensmann.com/license_de.html
  * All Rights Reserved. Use is subject to license terms, see http://www.bensmann.com/license_en.html
  * 
