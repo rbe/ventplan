7bb1f00130cad7c2676fe74d907531e304029932
Ralf Bensmann <ralf@art-of-coding.eu>
2010-07-19 16:27:06 +0200

    Added new event class

---
+++ griffon-app/events/com/westaflex/wac/RaumVsEvents.groovy
@@ -0,0 +1,50 @@
+/**
+ * /Users/rbe/project/wac2/griffon-app/events/com/westaflex/wac/RaumVsEvents.groovy
+ * 
+ * Copyright (C) 2010 Informationssysteme Ralf Bensmann.
+ * Alle Rechte vorbehalten. Nutzungslizenz siehe http://www.bensmann.com/license_de.html
+ * All Rights Reserved. Use is subject to license terms, see http://www.bensmann.com/license_en.html
+ * 
+ * Created by: rbe
+ */
+package com.westaflex.wac
+
+import com.bensmann.griffon.GriffonHelper as GH
+
+/**
+ * 
+ */
+@griffon.util.EventPublisher
+class RaumVsEvents {
+	
+	def model
+	def wacCalculationService
+	def wacModelService
+	
+	/**
+	 * Execute code 'later'.
+	 */
+	def doLater = { closure ->
+		javax.swing.SwingUtilities.invokeLater closure
+	}
+	
+	/**
+	 * Das Zentralgerät wurde geändert.
+	 */
+	def onZentralgeratGewahlt = { zentralgerat ->
+		// TODO rbe Compare old and new value; only change values when old != new?
+		doLater {
+			// Merken, dass das Zentralgerät manuell ausgewählt wurde
+			// -> keine automatische Auswahl des Zentralgeräts mehr durchführen
+			model.map.anlage.zentralgeratManuell = true
+			// Hole Volumenströme des Zentralgeräts
+			model.meta.volumenstromZentralgerat =
+				wacModelService.getVolumenstromFurZentralgerat(zentralgerat)
+			// Im Projekt-Model speichern
+			model.map.anlage.zentralgerat = zentralgerat
+			// Aussenluftvolumenströme neu berechnen
+			wacCalculationService.aussenluftVs(model.map)
+		}
+	}
+	
+}
