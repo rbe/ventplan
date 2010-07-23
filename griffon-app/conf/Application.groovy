5a2c0c2128ac0373b0d176f227fc9390aed7b225
Ralf Bensmann <ralf@art-of-coding.eu>
2010-07-23 11:55:44 +0200

    Inject gsql only in services

--- griffon-app/conf/Application.groovy
+++ griffon-app/conf/Application.groovy
@@ -1,21 +1,25 @@
 application {
-	title = "Wac2"
-	startupGroups = ["wac2"]
-	autoShutdown = false
+	title="Wac2"
+	startupGroups=["wac2"]
+	autoShutdown=false
 }
 mvcGroups {
 	Projekt {
-		model = "com.westaflex.wac.ProjektModel"
-		controller = "com.westaflex.wac.ProjektController"
-		actions = "com.westaflex.wac.ProjektActions"
-		view = "com.westaflex.wac.ProjektView"
+		model="com.westaflex.wac.ProjektModel"
+		controller="com.westaflex.wac.ProjektController"
+		actions="com.westaflex.wac.ProjektActions"
+		view="com.westaflex.wac.ProjektView"
 	}
 	wac2 {
-		model = "Wac2Model"
-		controller = "Wac2Controller"
-		actions = "Wac2Actions"
-		view = "Wac2View"
+		model="Wac2Model"
+		controller="Wac2Controller"
+		actions="Wac2Actions"
+		view="Wac2View"
 	}
 }
+griffon {
+	basic_injection.disable=true
+	gsql.injectInto=[/*"controller", */"service"]
+}
+
 griffon.basic_injection.disable = true
-griffon.gsql.injectInto = ["controller", "service"]
