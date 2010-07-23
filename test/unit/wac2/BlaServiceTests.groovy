21d26e3d41623986e74fbd8ef07ca01f61099981
Ralf Bensmann <ralf@art-of-coding.eu>
2010-07-23 13:28:20 +0200

    ...

---
+++ test/unit/wac2/BlaServiceTests.groovy
@@ -0,0 +1,17 @@
+package wac2
+
+import griffon.test.*
+
+class BlaServiceTests extends GriffonUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
