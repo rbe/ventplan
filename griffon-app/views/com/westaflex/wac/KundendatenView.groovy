5f1dbf7dbf7e6f053b2adf15f08d6026be0c653d
Ralf Bensmann <ralf@art-of-coding.eu>
2010-08-01 19:50:31 +0200

    Played with ScrollPane... added it around AnlagendatenView

--- griffon-app/views/com/westaflex/wac/KundendatenView.groovy
+++ griffon-app/views/com/westaflex/wac/KundendatenView.groovy
@@ -1,77 +1,87 @@
+/**
+ * /Users/rbe/project/wac2/griffon-app/views/com/westaflex/wac/KundendatenView.groovy
+ * 
+ * Copyright (C) 2010 Informationssysteme Ralf Bensmann.
+ * Alle Rechte vorbehalten. Nutzungslizenz siehe http://www.bensmann.com/license_de.html
+ * All Rights Reserved. Use is subject to license terms, see http://www.bensmann.com/license_en.html
+ * 
+ * Created by: rbe
+ */
 package com.westaflex.wac
 
 import com.bensmann.griffon.GriffonHelper as GH
 import net.miginfocom.swing.MigLayout
 
 //panel(id: "kundendatenHauptPanel", layout: new MigLayout("wrap 2", "[fill,grow]", "[fill,grow]")) {
-    // Großhandel
-    panel(id: "kundendatenGrosshandel", border: titledBorder(title: "Kunde 1 (Großhandel)"), layout: new MigLayout("fill, insets 5, gap 5, wrap 2", "[][grow]", "[][]")) {
-        // Row 1
-        label("Firma 1")
-        textField(id: "grosshandelFirma1", constraints: "growx")
-        // Row 2
-        label("Firma 2")
-        textField(id: "grosshandelFirma2", constraints: "growx")
-        // Row 3
-        label("Strasse")
-        textField(id: "grosshandelStrasse", constraints: "growx")
-        // Row 4
-        label("PLZ / Ort")
-        panel(constraints: "grow", layout: new MigLayout("fill, insets 2, gap 2", "[grow][grow]", "[grow]")) {
-            textField(id: "grosshandelPlz", constraints: "growx")
-            textField(id: "grosshandelOrt", constraints: "growx")
-        }
-        // Row 5
-        label("Telefon")
-        textField(id: "grosshandelTelefon", constraints: "growx")
-        // Row 6
-        label("Telefax")
-        textField(id: "grosshandelTelefax", constraints: "growx")
-        // Row 7
-        label("Ansprechpartner")
-        textField(id: "grosshandelAnsprechpartner", constraints: "growx")
-    }
-    GH.recurse(kundendatenGrosshandel, GH.yellowTextField)
-    // Ausführende Firma
-    panel(id: "kundendatenAusfuhrendeFirma", border: titledBorder(title: "Kunde 2 (Ausführende Firma)"), layout: new MigLayout("fill, insets 5, gap 5, wrap 2", "[][grow]", "[][]")) {
-        // Row 1
-        label("Firma 1")
-        textField(id: "ausfuhrendeFirmaFirma1", constraints: "growx")
-        // Row 2
-        label("Firma 2")
-        textField(id: "ausfuhrendeFirmaFirma2", constraints: "growx")
-        // Row 3
-        label("Strasse")
-        textField(id: "ausfuhrendeFirmaStrasse", constraints: "growx")
-        // Row 4
-        label("PLZ / Ort")
-        panel(constraints: "grow", layout: new MigLayout("fill, insets 2, gap 2", "[grow][grow]", "[grow]")) {
-            textField(id: "ausfuhrendeFirmaPlz", constraints: "growx")
-            textField(id: "ausfuhrendeFirmaOrt", constraints: "growx")
-        }
-        // Row 5
-        label("Telefon")
-        textField(id: "ausfuhrendeFirmaTelefon", constraints: "growx")
-        // Row 6
-        label("Telefax")
-        textField(id: "ausfuhrendeFirmaTelefax", constraints: "growx")
-        // Row 7
-        label("Ansprechpartner")
-        textField(id: "ausfuhrendeFirmaAnsprechpartner", constraints: "growx")
-    }
-    GH.recurse(kundendatenAusfuhrendeFirma, GH.yellowTextField)
-    // Notizen
-    panel(id: "kundendatenNotizen", border: titledBorder(title: "Notizen"), constraints: "span", layout: new MigLayout("fill, wrap 2", "[][grow]", "[][grow]")) {
-        // Bauvorhaben
-        label("Bauvorhaben")
-        textField(id: "bauvorhaben", constraints: "growx")
-        // Notizen
-        label("Notizen")
-        jideScrollPane(constraints: "grow") {
-            textArea(id: "notizen", rows: 40, constraints: "grow")
-        }
-    }
-    GH.recurse(kundendatenNotizen, GH.yellowTextField)
-//}
+
+// Großhandel
+panel(id: "kundendatenGrosshandel", border: titledBorder(title: "Kunde 1 (Großhandel)"), layout: new MigLayout("fill, insets 5, gap 5, wrap 2", "[][grow]", "[][]")) {
+	// Row 1
+	label("Firma 1")
+	textField(id: "grosshandelFirma1", constraints: "growx")
+	// Row 2
+	label("Firma 2")
+	textField(id: "grosshandelFirma2", constraints: "growx")
+	// Row 3
+	label("Strasse")
+	textField(id: "grosshandelStrasse", constraints: "growx")
+	// Row 4
+	label("PLZ / Ort")
+	panel(constraints: "grow", layout: new MigLayout("fill, insets 2, gap 2", "[grow][grow]", "[grow]")) {
+		textField(id: "grosshandelPlz", constraints: "growx")
+		textField(id: "grosshandelOrt", constraints: "growx")
+	}
+	// Row 5
+	label("Telefon")
+	textField(id: "grosshandelTelefon", constraints: "growx")
+	// Row 6
+	label("Telefax")
+	textField(id: "grosshandelTelefax", constraints: "growx")
+	// Row 7
+	label("Ansprechpartner")
+	textField(id: "grosshandelAnsprechpartner", constraints: "growx")
+}
+GH.recurse(kundendatenGrosshandel, GH.yellowTextField)
+// Ausführende Firma
+panel(id: "kundendatenAusfuhrendeFirma", border: titledBorder(title: "Kunde 2 (Ausführende Firma)"), layout: new MigLayout("fill, insets 5, gap 5, wrap 2", "[][grow]", "[][]")) {
+	// Row 1
+	label("Firma 1")
+	textField(id: "ausfuhrendeFirmaFirma1", constraints: "growx")
+	// Row 2
+	label("Firma 2")
+	textField(id: "ausfuhrendeFirmaFirma2", constraints: "growx")
+	// Row 3
+	label("Strasse")
+	textField(id: "ausfuhrendeFirmaStrasse", constraints: "growx")
+	// Row 4
+	label("PLZ / Ort")
+	panel(constraints: "grow", layout: new MigLayout("fill, insets 2, gap 2", "[grow][grow]", "[grow]")) {
+		textField(id: "ausfuhrendeFirmaPlz", constraints: "growx")
+		textField(id: "ausfuhrendeFirmaOrt", constraints: "growx")
+	}
+	// Row 5
+	label("Telefon")
+	textField(id: "ausfuhrendeFirmaTelefon", constraints: "growx")
+	// Row 6
+	label("Telefax")
+	textField(id: "ausfuhrendeFirmaTelefax", constraints: "growx")
+	// Row 7
+	label("Ansprechpartner")
+	textField(id: "ausfuhrendeFirmaAnsprechpartner", constraints: "growx")
+}
+GH.recurse(kundendatenAusfuhrendeFirma, GH.yellowTextField)
+// Notizen
+panel(id: "kundendatenNotizen", border: titledBorder(title: "Notizen"), constraints: "span", layout: new MigLayout("fill, wrap 2", "[][grow]", "[][grow]")) {
+	// Bauvorhaben
+	label("Bauvorhaben")
+	textField(id: "bauvorhaben", constraints: "growx")
+	// Notizen
+	label("Notizen")
+	jideScrollPane(constraints: "grow") {
+		textArea(id: "notizen", rows: 40, constraints: "grow")
+	}
+}
+GH.recurse(kundendatenNotizen, GH.yellowTextField)
+
 // Bindings
 build(KundendatenBindings)
--- griffon-app/views/com/westaflex/wac/ProjektView.groovy
+++ griffon-app/views/com/westaflex/wac/ProjektView.groovy
@@ -11,61 +11,55 @@ package com.westaflex.wac
 
 import net.miginfocom.swing.MigLayout
 
-
-
 jideTabbedPane(projektTabGroup, selectedIndex: projektTabGroup.tabCount) {
+	
 	panel(id: "projektTab", title: controller.makeTabTitle()) {
 		borderLayout()
-		// Scrollpane
-		//jideScrollPane() {
-			jideTabbedPane(id: "datenTabGroup") {
-				// Kundendaten
-				panel(id: "kundenTab", title: "Kundendaten", layout: new MigLayout("fillx, wrap 2", "[fill][fill]", "[fill][fill]")) {
-					build(KundendatenView)
-				}
-				// Gebäudedaten
-				panel(id: "gebaudeTab", title: "Gebäudedaten", layout: new MigLayout("fillx, wrap 3", "[fill][fill][fill]", "[fill][fill]")) {
-					build(GebaudedatenView)
-				}
-				// Anlagendaten
-				panel(id: "anlageTab", title: "Anlagedaten", layout: new MigLayout("fillx, wrap 3", "[fill][fill][fill]", "[fill][fill]")) {
-					build(AnlagendatenView)
-				}
-				// Raumdaten
-				panel(id: "raumTab", title: "Raumdaten", layout: new MigLayout("fillx")) {
-					build(RaumdatenView)
-				}
-				// Außenluftvolumenströme
-				panel(id: "aussenluftVsTab", title: "Außenluftvolumenströme", layout: new MigLayout("fillx, wrap", "[fill]", "[fill]")) {
-					build(AussenluftVsView)
-				}
-				// Raumvolumenströme
-				panel(id: "raumVsTab", title: "Raumvolumenströme", layout: new MigLayout("fillx")) {
-					build(RaumVsView)
-				}
-				// Druckverlustberechnung
-				panel(id: "dvbTab", title: "Druckverlustberechnung") {
-					build(DruckverlustView)
-				}
-				// Akkustikberechnung
-				panel(id: "akustikTab", title: "Akustikberechnung") {
-					build(AkustikView)
-				}
-				
-				// RaumdatenDialogView - Testweise hier...
-				//panel(id: "raumdatenDialogTab", title: "RaumdatenDialog Einstellungen") {
-				//	build(RaumdatenDialogView)
-				//}
+		jideTabbedPane(id: "datenTabGroup") {
+			// Kundendaten
+			panel(id: "kundenTab", title: "Kundendaten", layout: new MigLayout("fillx, wrap 2", "[fill][fill]", "[fill][fill]")) {
+				build(KundendatenView)
 			}
-		//}
+			// Gebäudedaten
+			panel(id: "gebaudeTab", title: "Gebäudedaten", layout: new MigLayout("fillx, wrap 3", "[fill][fill][fill]", "[fill][fill]")) {
+				build(GebaudedatenView)
+			}
+			// Anlagendaten
+			panel(id: "anlageTab", title: "Anlagedaten", layout: new MigLayout("fillx, wrap 3", "[fill][fill][fill]", "[fill][fill]")) {
+				jideScrollPane() { build(AnlagendatenView) }
+			}
+			// Raumdaten
+			panel(id: "raumTab", title: "Raumdaten", layout: new MigLayout("fillx")) {
+				build(RaumdatenView)
+			}
+			// Außenluftvolumenströme
+			panel(id: "aussenluftVsTab", title: "Außenluftvolumenströme", layout: new MigLayout("fillx, wrap", "[fill]", "[fill]")) {
+				build(AussenluftVsView)
+			}
+			// Raumvolumenströme
+			panel(id: "raumVsTab", title: "Raumvolumenströme", layout: new MigLayout("fillx")) {
+				build(RaumVsView)
+			}
+			// Druckverlustberechnung
+			panel(id: "dvbTab", title: "Druckverlustberechnung") {
+				build(DruckverlustView)
+			}
+			// Akkustikberechnung
+			panel(id: "akustikTab", title: "Akustikberechnung") {
+				build(AkustikView)
+			}
+		}
+		/*
 		hbox(constraints: SOUTH) {
 			//button("Hier könnte Ihre Werbung stehen!")
 		}
+		*/
 	}
+	
 }
 // Bindings
 build(ProjektBindings)
-
+// TODO mmu Documentation
 def event
 //
 // JIDE
