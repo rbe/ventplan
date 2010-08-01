05594c9de9ab79a23d0682a49c129a1c62a17799
Ralf Bensmann <ralf@art-of-coding.eu>
2010-08-01 19:50:08 +0200

    Added XmlHelper

---
+++ griffon-app/util/com/bensmann/griffon/XmlHelper.groovy
@@ -0,0 +1,79 @@
+/**
+ * /Users/rbe/project/wac2/griffon-app/util/com/bensmann/griffon/XmlHelper.groovy
+ * 
+ * Copyright (C) 2010 Informationssysteme Ralf Bensmann.
+ * Alle Rechte vorbehalten. Nutzungslizenz siehe http://www.bensmann.com/license_de.html
+ * All Rights Reserved. Use is subject to license terms, see http://www.bensmann.com/license_en.html
+ * 
+ * Created by: rbe
+ */
+package com.bensmann.griffon
+
+class XmlHelper {
+	
+	/**
+	 * Groovy DOM builder.
+	 */
+	def static domBuilder
+	
+	/**
+	 * Try to create a node.
+	 */
+	def static tc = { valueClosure, defaultClosure = null ->
+		try {
+			valueClosure()
+		} catch (e) {
+			//
+			println "tc: CATCHED: ${e}"
+			// Default?
+			if (defaultClosure) {
+				defaultClosure()
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	def static m = { keys, map ->
+		if (map) {
+			keys.each { k ->
+				XmlHelper.tc { domBuilder."${k}"(map[k] ?: "") } { XmlHelper.domBuilder."${k}"() }
+			}
+		}
+	}
+	
+	def static vs = { closure ->
+		def std = ""
+		try {
+			return closure() ?: std
+		} catch (e) { println e }
+		std
+	}
+	
+	def static vi = { closure ->
+		def std = 0
+		try {
+			return closure() ?: std
+		} catch (e) { println e }
+		std
+	}
+	
+	def static vd = { closure ->
+		def std = 0.0d
+		try {
+			return closure() ?: std
+		} catch (e) { println e }
+		std
+	}
+	
+	def static vb = { closure ->
+		def std = false
+		try {
+			return closure() ?: std
+		} catch (e) { println e }
+		std
+	}
+	
+	
+}
