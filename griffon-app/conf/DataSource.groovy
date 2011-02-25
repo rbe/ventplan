1253f0ab87b4a7154dc3ac2458159a8bdbc55167
Ralf Bensmann <ralf@art-of-coding.eu>
2011-02-25 15:10:03 +0100

    Added zipped H2 database (for read-only DB)

--- griffon-app/conf/DataSource.groovy
+++ griffon-app/conf/DataSource.groovy
@@ -1,5 +1,6 @@
 dataSource {
-	url = "jdbc:h2:sql/westawac" //"jdbc:mysql://localhost:3306/Westa" //"jdbc:hsqldb:file:devDB;shutdown=true"
+	//url = "jdbc:h2:sql/westawac"
+	url = "jdbc:h2:zip:sql/westawac_db.zip!/westawac"
 	driverClassName = "org.h2.Driver" //"com.mysql.jdbc.Driver" //"org.hsqldb.jdbcDriver"
 	username = "sa"
 	password = ""
