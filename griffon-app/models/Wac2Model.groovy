2002c4f23fc23c8b2a8503e1f8d6ee9fc2a67034
Ralf Bensmann <ralf@art-of-coding.eu>
2011-03-15 10:38:09 +0100

    Korrektur von prufeRaumdaten(), ZU/AB-Räume wurden nicht richtig berücksichtigt

--- griffon-app/models/Wac2Model.groovy
+++ griffon-app/models/Wac2Model.groovy
@@ -1,11 +1,13 @@
-/**
- * /Users/rbe/project/westaflex/WestaWAC2/griffon-app/models/Wac2Model.groovy
- * 
- * Copyright (C) 2010 Informationssysteme Ralf Bensmann.
- * Nutzungslizenz siehe http://www.bensmann.com/BPL_v10_de.html
- * Use is subject to license terms, see http://www.bensmann.com/BPL_v10_en.html
- * 
- * Created by: rbe
+/*
+ * Copyright (C) 2009-2010 Informationssysteme Ralf Bensmann.
+ * Copyright (C) 2010-2011 art of coding UG (haftungsbeschränkt).
+ *
+ * Nutzungslizenz siehe http://files.art-of-coding.eu/aoc/AOCPL_v10_de.html
+ * Use is subject to license terms, see http://files.art-of-coding.eu/aoc/AOCPL_v10_en.html
+ *
+ * Project wac
+ * /Users/rbe/project/wac/griffon-app/models/Wac2Model.groovy
+ * Last modified at 14.03.2011 20:27:07 by rbe
  */
 import groovy.beans.Bindable
 
@@ -53,5 +55,10 @@ class Wac2Model {
 	 * Wurde das Model des aktuellen Projekts geändert?
 	 */
 	@Bindable Boolean aktivesProjektGeandert = false
+
+    /**
+     * Wurde irgendein Model eines Projekts geändert?
+     */
+    @Bindable Boolean alleProjekteGeandert = false
 	
 }
