73d51aca34e39e5f2162db92b7e8df6684583fed
Ralf Bensmann <ralf@art-of-coding.eu>
2011-03-22 14:51:22 +0100

    ...

--- griffon-app/conf/metainf/spring/springbeans.groovy
+++ griffon-app/conf/metainf/spring/springbeans.groovy
@@ -1,2 +1,14 @@
+/*
+ * Copyright (C) 2009-2010 Informationssysteme Ralf Bensmann.
+ * Copyright (C) 2010-2011 art of coding UG (haftungsbeschränkt).
+ *
+ * Nutzungslizenz siehe http://files.art-of-coding.eu/aoc/AOCPL_v10_de.html
+ * Use is subject to license terms, see http://files.art-of-coding.eu/aoc/AOCPL_v10_en.html
+ *
+ * Project wac
+ * /Users/rbe/project/wac/griffon-app/conf/metainf/spring/springbeans.groovy
+ * Last modified at 22.03.2011 14:50:12 by rbe
+ */
+
 // This file is empty.
-beans = {}
+// beans = {}
