b70c92087dc33be03057e871e4b2f88390bf6d54
Ralf Bensmann <ralf@art-of-coding.eu>
2011-04-04 20:34:10 +0200

    ...

--- griffon-app/conf/DataSource.groovy
+++ griffon-app/conf/DataSource.groovy
@@ -1,5 +1,7 @@
 dataSource {
-    // For development url = "jdbc:h2:zip:../lib/dtmp.zip!/westawac"
+    // For development
+    //url = "jdbc:h2:zip:../lib/dtmp.zip!/westawac"
+    // For distribution
 	url = "jdbc:h2:zip:lib/dtmp.zip!/westawac"
 	driverClassName = "org.h2.Driver" //"com.mysql.jdbc.Driver" //"org.hsqldb.jdbcDriver"
 	username = "sa"
