47b6f46d093fb2f916b5a23d38c877bba3524e4e
Ralf Bensmann <ralf@art-of-coding.eu>
2011-04-09 20:11:28 +0200

    Code reformat

--- griffon-app/controllers/com/westaflex/wac/DialogController.groovy
+++ griffon-app/controllers/com/westaflex/wac/DialogController.groovy
@@ -14,55 +14,55 @@ import com.ezware.dialog.task.CommandLink
  * Manage and create dialogs.
  */
 class DialogController {
-	
-	/**
-	 * Dialog anzeigen, wenn die Applikation geschlossen werden soll, obwohl
-	 * noch nicht gespeicherte Projekte vorhanden sind.
-	 */
-	def showApplicationCloseDialog() {
-		def choice = choice(
-						"Anwendung schliessen?",
-						"Die Anwendung enthält nicht gespeicherte Projekte. Bitte wählen Sie.",
-						1,
-						[
-							new CommandLink("Alle Speichern", ""),
-							//new CommandLink("Abbrechen", ""),
-							new CommandLink("Ohne Speichern schliessen", "")
-						]
-					)
-		choice
-	}
-	
-	/**
-	 * Dialog anzeigen, wenn ein nicht gespeichertes Projekt geschlossen werden soll.
-	 */
-	def showCloseProjectDialog() {
-		def choice = choice(
-						"Projekt schliessen?",
-						"Das Projekt enthält nicht gespeicherte Werte. Bitte wählen Sie.",
-						1,
-						[
-							new CommandLink("Speichern", ""),
-							new CommandLink("Abbrechen", ""),
-							new CommandLink("Schliessen", "")
-						]
-					)
-		choice
-	}
-	
-	/**
-	 * Zeige Informationsdialog mit mitgegebener Nachricht an.
-	 */
-	def showInformDialog = { infoMsg ->
-		inform("Information", infoMsg)
-	}
-	
-	/**
-	 * Zeige Fehler-Informationsdialog mit mitgegebener Nachricht an.
-	 */
-	def showErrorDialog = { errorMsg ->
-		inform("Fehler", errorMsg)
-	}
+
+    /**
+     * Dialog anzeigen, wenn die Applikation geschlossen werden soll, obwohl
+     * noch nicht gespeicherte Projekte vorhanden sind.
+     */
+    def showApplicationCloseDialog() {
+        def choice = choice(
+                "Anwendung schliessen?",
+                "Die Anwendung enthält nicht gespeicherte Projekte. Bitte wählen Sie.",
+                1,
+                [
+                    new CommandLink("Alle Speichern", ""),
+                    //new CommandLink("Abbrechen", ""),
+                    new CommandLink("Ohne Speichern schliessen", "")
+                ]
+                )
+        choice
+    }
+
+    /**
+     * Dialog anzeigen, wenn ein nicht gespeichertes Projekt geschlossen werden soll.
+     */
+    def showCloseProjectDialog() {
+        def choice = choice(
+                "Projekt schliessen?",
+                "Das Projekt enthält nicht gespeicherte Werte. Bitte wählen Sie.",
+                1,
+                [
+                    new CommandLink("Speichern", ""),
+                    new CommandLink("Abbrechen", ""),
+                    new CommandLink("Schliessen", "")
+                ]
+                )
+        choice
+    }
+
+    /**
+     * Zeige Informationsdialog mit mitgegebener Nachricht an.
+     */
+    def showInformDialog = { infoMsg ->
+        inform("Information", infoMsg)
+    }
+
+    /**
+     * Zeige Fehler-Informationsdialog mit mitgegebener Nachricht an.
+     */
+    def showErrorDialog = { errorMsg ->
+        inform("Fehler", errorMsg)
+    }
 
     /**
      * Ticket #97
@@ -71,15 +71,15 @@ class DialogController {
      * Nein = es wird ein Blanko Angebot geöffnet
      */
     def showPrintProjectDialog() {
-		def choice = choice(
-						"Angebot aus der aktuellen Auslegung erstellen?",
-						"Bitte wählen Sie.",
-						1,
-						[
-							new CommandLink("Ja", ""),
-							new CommandLink("Nein", ""),
-						]
-					)
-		choice
-	}
+        def choice = choice(
+                "Angebot aus der aktuellen Auslegung erstellen?",
+                "Bitte wählen Sie.",
+                1,
+                [
+                    new CommandLink("Ja", ""),
+                    new CommandLink("Nein", ""),
+                ]
+                )
+        choice
+    }
 }
