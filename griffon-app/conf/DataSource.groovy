ff8e978f4969fe3e1b7dbd460edf4abb91f834c9
Manuel Murawski <manuel@art-of-coding.eu>
2011-12-10 17:41:02 +0100

    Added environments dev,test and prod for database url

--- griffon-app/conf/DataSource.groovy
+++ griffon-app/conf/DataSource.groovy
@@ -2,8 +2,25 @@ dataSource {
     // For development
     //url = "jdbc:h2:zip:../lib/dtmp.zip!/westawac"
     // For distribution
-    url = "jdbc:h2:zip:lib/dtmp.zip!/westawac"
+    //url = "jdbc:h2:zip:lib/dtmp.zip!/westawac"
     driverClassName = "org.h2.Driver" //"com.mysql.jdbc.Driver" //"org.hsqldb.jdbcDriver"
     username = "sa"
     password = ""
+}
+environments {  
+    development {  
+        dataSource {  
+            url = "jdbc:h2:zip:../lib/dtmp.zip!/westawac"
+        }  
+    }  
+    test {  
+        dataSource {  
+            url = "jdbc:h2:zip:../lib/dtmp.zip!/westawac"
+        }  
+    }  
+    production {  
+        dataSource {  
+            url = "jdbc:h2:zip:lib/dtmp.zip!/westawac"
+        }  
+    }  
 }
