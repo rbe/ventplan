f3bc21589f76120df90b7c5c56f0f1fa2cb01896
Ralf Bensmann <ralf@art-of-coding.eu>
2012-01-13 10:15:12 +0100

    OpenOffice not included in distribution anymore, cleanup binaries

--- bin/wac.bat
+++ bin/wac.bat
@@ -1,46 +1,21 @@
 @echo off
 rem
-rem /Users/rbe/project/wac2/wac.bat
+rem Copyright (C) 2009-2010 Informationssysteme Ralf Bensmann.
+rem Copyright (C) 2011-2012 art of coding UG (haftungsbeschränkt).
 rem 
-rem Copyright (C) 2010 Informationssysteme Ralf Bensmann.
-rem Nutzungslizenz siehe http://www.bensmann.com/BPL_v10_de.html
-rem Use is subject to license terms, see http://www.bensmann.com/BPL_v10_en.html
-rem 
+rem Alle Rechte vorbehalten. All rights reserved.
 rem Created by: rbe
 rem
 
 rem Java 6
 set JAVA_HOME=Windows\jre6
 set PATH=%JAVA_HOME%\bin;%PATH%
-
-rem Use OpenOffice Portable -- batteries included.
-set OOO_HOME=Windows\OpenOfficePortable\App\openoffice
-set OOO_PROGRAM=%OOO_HOME%\program
-
-rem Java Classpath
-set OOOJ=%OOO_HOME%\URE\java
-set OOOU=%OOO_HOME%\Basis\program\classes
-set OOOL=%OOO_HOME%\Basis\program;%OOO_HOME%\program\components;%OOO_HOME%\URE\bin
-
-rem Set UNO path
-set UNO_PATH=%OOO_PROGRAM%
-
-rem Copy juh.jar into program folder
-rem http://user.services.openoffice.org/en/forum/viewtopic.php?f=44&t=10825
-copy "%OOOJ%\juh.jar" "%OOO_PROGRAM%" 1>nul 2>&1
-
-rem Classpath
-set CLASSPATH=%OOO_PROGRAM%\juh.jar;%OOOJ%\jurt.jar;%OOOJ%\ridl.jar;%OOOU%\unoil.jar;lib\*
-
-rem No OpenOffice running
-rem taskkill /f /im soffice* 1>nul 2>&1
-
+set CLASSPATH=lib\*
 rem Update
-xcopy update\conf\* conf /e /c /i /f /r /y 1>%TEMP%\wac.log 2>&1
+xcopy update\resources\* resources /e /c /i /f /r /y 1>%TEMP%\wac.log 2>&1
 xcopy update\sql\* sql /e /c /i /f /r /y 1>>%TEMP%\wac.log 2>&1
 xcopy update\lib\* lib /e /c /i /f /r /y 1>>%TEMP%\wac.log 2>&1
 rmdir /s /q update 1>>%TEMP%\wac.log 2>&1
-
 rem Start
-start javaw -cp "%CLASSPATH%" -Djava.library.path="%OOOL%" griffon.swing.SwingApplication 1>>%TEMP%\wac.log 2>&1
+start javaw -cp "%CLASSPATH%" griffon.swing.SwingApplication 1>>%TEMP%\wac.log 2>&1
 stop
