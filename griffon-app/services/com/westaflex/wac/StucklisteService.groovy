46de3ccde1197299f63d6870870e020c7324c0c8
Ralf Bensmann <ralf@art-of-coding.eu>
2012-03-03 23:25:04 +0100

    WAC-108

--- griffon-app/services/com/westaflex/wac/StucklisteService.groovy
+++ griffon-app/services/com/westaflex/wac/StucklisteService.groovy
@@ -1,13 +1,14 @@
 /*
- * WAC
+ * VentPlan
  *
- * Copyright (C) 2005      Informationssysteme Ralf Bensmann.
- * Copyright (C) 2009-2010 Informationssysteme Ralf Bensmann.
+ * Copyright (C) 2005-2010 Informationssysteme Ralf Bensmann.
  * Copyright (C) 2011-2012 art of coding UG (haftungsbeschränkt).
  *
  * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
  * All rights reserved. Use is subject to license terms.
  */
+
+
 package com.westaflex.wac
 
 import groovy.sql.Sql
