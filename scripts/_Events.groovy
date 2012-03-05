269fbcd7d35166272ebc48b4584e3ff7ea4e2c2b
Manuel Murawski <manuel@art-of-coding.eu>
2012-03-05 23:30:03 +0100

    Updated installer. Generating .exe file with jsmooth.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -8,7 +8,16 @@
  * All rights reserved. Use is subject to license terms.
  */
 
+/**
+ * IZPACK
+ * Erstellen mit 'griffon prod package izpack'
+ *
+ * JSMOOTH
+ * Erstellen mit 'griffon prod create-windows'
+ */
+
 eventPackageResourcesEnd = {
+    // IZPACK START
     // Copy customized izpack files from /src/main/installer/izpack/resources to izpack resources dir.
     ant.copy( todir: "${projectWorkDir}/installer/izpack/resources", overwrite: true ) {
         fileset( dir: "${basedir}/src/main/installer/izpack/resources", includes: "**" )
@@ -25,11 +34,38 @@ eventPackageResourcesEnd = {
     ant.copy( todir: "${projectWorkDir}/installer/izpack/resources", overwrite: true ) {
         fileset( dir: "${basedir}/griffon-app/resources/image", includes: "*.png" )
     }
+    // IZPACK END
+
+    // JSMOOTH START
+    ant.copy( todir: "${projectWorkDir}/installer/jsmooth/dist/sql", overwrite: true ) {
+        fileset( dir: "${basedir}/sql", includes: "*.db" )
+    }
+    ant.copy( todir: "${projectWorkDir}/installer/jsmooth/dist/bin", overwrite: true ) {
+        fileset( dir: "${basedir}/bin", includes: "**" )
+    }
+    ant.copy( todir: "${projectWorkDir}/installer/jsmooth", overwrite: true ) {
+        fileset( dir: "${basedir}/src/main/installer/izpack/resources", includes: "*.jsmooth" )
+    }
+    // JSMOOTH END
 }
 
 eventCreatePackageStart = {
+    // IZPACK START
     // Copy customized VentPlan executables from /bin to izpack bin dir.
     ant.copy( todir: "${projectWorkDir}/installer/izpack/binary/bin", overwrite: true ) {
         fileset( dir: "${basedir}/bin", includes: "**" )
     }
+    ant.copy( todir: "${projectWorkDir}/installer/jsmooth/dist/bin", overwrite: true ) {
+        fileset( dir: "${basedir}/bin", includes: "**" )
+    }
+    ant.copy( todir: "${projectWorkDir}/installer/jsmooth", overwrite: true ) {
+        fileset( dir: "${basedir}/src/main/installer/izpack/resources", includes: "*.jsmooth" )
+    }
+    // IZPACK END
+
+    // JSMOOTH START
+    ant.copy( todir: "${basedir}/dist/windows/bin", overwrite: true ) {
+        fileset( dir: "${basedir}/bin", includes: "**" )
+    }
+    // JSMOOTH END
 }
