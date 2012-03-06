899aa9369d223d3f15e9128a42dff9380adf41a0
Ralf Bensmann <ralf@art-of-coding.eu>
2012-03-06 21:49:15 +0100

    Added use of VentPlan properties for URL

--- griffon-app/views/com/westaflex/wac/CheckUpdateView.groovy
+++ griffon-app/views/com/westaflex/wac/CheckUpdateView.groovy
@@ -7,8 +7,6 @@
  * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
  * All rights reserved. Use is subject to license terms.
  */
-
-
 package com.westaflex.wac
 
 import net.miginfocom.swing.MigLayout
@@ -16,14 +14,15 @@ import com.bensmann.griffon.GriffonHelper as GH
 
 // About view
 panel(layout: new MigLayout("wrap", "[center]", "[fill]"), constraints: "grow") {
-    label(icon: imageIcon("/image/ventplan_splash.png"), constraints: "wrap")
+    label(icon: imageIcon('/image/ventplan_splash.png'), constraints: 'wrap')
 
     label(' ')
-    label("Es liegt ein Update bereit!")
+    label('Es liegt ein Update bereit!')
 
     label(' ')
-    label(id: '', 'http://www.ventplan.com/latest/', mouseReleased: { e ->
-        java.awt.Desktop.desktop.browse(java.net.URI.create('http://www.ventplan.com/latest/'))
+    label('http://www.ventplan.com/latest/', mouseReleased: { e ->
+        String url = GH.getVentplanProperties().get('ventplan.update.info.url')
+        java.awt.Desktop.desktop.browse(java.net.URI.create(url))
     })
     /*
     label(' ')
