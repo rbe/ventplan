35e54f215c8787e653b900f484fd038fa4dbb228
Ralf Bensmann <ralf@art-of-coding.eu>
2012-04-19 10:56:57 +0200

    Changed name, version

--- griffon-app/conf/Application.groovy
+++ griffon-app/conf/Application.groovy
@@ -9,24 +9,24 @@
  */
 
 application {
-	title='Wac2'
-	startupGroups=['wac2', 'Dialog']
-	autoShutdown=false
+    title = 'VentPlan'
+    startupGroups = ['wac2', 'Dialog']
+    autoShutdown = false
 }
 mvcGroups {
-	'Projekt' {
-		model="com.westaflex.wac.ProjektModel"
-		controller="com.westaflex.wac.ProjektController"
-		actions="com.westaflex.wac.ProjektActions"
-		view="com.westaflex.wac.ProjektView"
-	}
-	'wac2' {
-		model="com.westaflex.wac.Wac2Model"
-		controller="com.westaflex.wac.Wac2Controller"
-		actions="com.westaflex.wac.Wac2Actions"
-		view="com.westaflex.wac.Wac2View"
-	}
-	Dialog {
-		controller="com.westaflex.wac.DialogController"
-	}
+    'Projekt' {
+        model = 'com.westaflex.wac.ProjektModel'
+        controller = 'com.westaflex.wac.ProjektController'
+        actions = 'com.westaflex.wac.ProjektActions'
+        view = 'com.westaflex.wac.ProjektView'
+    }
+    'wac2' {
+        model = 'com.westaflex.wac.Wac2Model'
+        controller = 'com.westaflex.wac.Wac2Controller'
+        actions = 'com.westaflex.wac.Wac2Actions'
+        view = 'com.westaflex.wac.Wac2View'
+    }
+    Dialog {
+        controller = 'com.westaflex.wac.DialogController'
+    }
 }
