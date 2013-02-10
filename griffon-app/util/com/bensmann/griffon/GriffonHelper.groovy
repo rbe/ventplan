89244f4b004eef93a7a4ac25eba1a4ab41eca93b
Manuel Murawski <manuel.murawski@art-of-coding.eu>
2013-02-10 20:27:39 +0100

    WAC-240: Added support for listing images with text in combo box list.

--- griffon-app/util/com/bensmann/griffon/GriffonHelper.groovy
+++ griffon-app/util/com/bensmann/griffon/GriffonHelper.groovy
@@ -13,6 +13,7 @@ package com.bensmann.griffon
 
 import griffon.transform.Threading
 
+import javax.swing.plaf.basic.BasicComboBoxRenderer
 import java.awt.Color
 import java.math.RoundingMode
 
@@ -541,13 +542,18 @@ class GriffonHelper {
 
     /**
      * Make a CellEditor with a ComboBox for GlazedLists.
+     * WAC-240: Abbildungen Ventile. Added imagesupport parameter to set custom combobox renderer.
      */
-    static void makeComboboxCellEditor(column, list) {
+    static void makeComboboxCellEditor(column, list, imagesupport = false) {
         javax.swing.SwingUtilities.invokeLater {
             def eventList = ca.odell.glazedlists.GlazedLists.eventList(list) as ca.odell.glazedlists.EventList
             def threadEventList = ca.odell.glazedlists.GlazedLists.threadSafeList(eventList)
-            javax.swing.DefaultCellEditor cellEditor = ca.odell.glazedlists.swing.AutoCompleteSupport.createTableCellEditor(threadEventList)
-            column.setCellEditor(cellEditor)
+            def cellEditor = ca.odell.glazedlists.swing.AutoCompleteSupport.createTableCellEditor(threadEventList)
+            // WAC-240: set custom renderer for combobox (label with image).
+            if (imagesupport) {
+                cellEditor.getAutoCompleteSupport().getComboBox().setRenderer(new ComboBoxImageRenderer())
+            }
+            column.setCellEditor((javax.swing.DefaultCellEditor) cellEditor)
         }
     }
 
