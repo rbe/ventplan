7fbdd18abd0bdb038aec0806391d742f7c460826
Manuel Murawski <manuel.murawski@art-of-coding.eu>
2013-04-02 23:02:09 +0200

    Hide last column in table. Updated sizes.

--- griffon-app/views/eu/artofcoding/ventplan/desktop/WbwView.groovy
+++ griffon-app/views/eu/artofcoding/ventplan/desktop/WbwView.groovy
@@ -14,12 +14,14 @@ package eu.artofcoding.ventplan.desktop
 import eu.artofcoding.griffon.helper.GriffonHelper as GH
 import net.miginfocom.swing.MigLayout
 
+import javax.swing.ListSelectionModel
+import javax.swing.JTable
 import java.awt.*
 
-panel(id: 'wbwPanel', layout: new MigLayout('fillx, wrap 2', '[fill][fill]', '[fill][fill]')) {
+panel(id: 'wbwPanel', layout: new MigLayout('fillx, wrap 2', '[fill]', '[fill]0[fill]')) {
 
     // Links oben: Tabelle: Anzahl (Textfeld), Bezeichnung des Widerstands, Widerstandswert
-    panel(id: 'wbwTabellePanel', layout: new MigLayout('fill', '[fill]', '[fill]')) {
+    panel(id: 'wbwTabellePanel', layout: new MigLayout('fill', '[fill]', '[fill]0[fill]')) {
         jideScrollPane() {
             table(id: 'wbwTabelle', model: model.createWbwTableModel(), selectionMode: ListSelectionModel.SINGLE_SELECTION) {
                 // WAC-160: Feste Spaltenbreiten vergeben.
@@ -27,8 +29,12 @@ panel(id: 'wbwPanel', layout: new MigLayout('fillx, wrap 2', '[fill][fill]', '[f
                 //current.removeColumn(current.columnModel.getColumn(0)) // REMOVE
                 current.columnModel.getColumn(0).setPreferredWidth(60)
                 current.columnModel.getColumn(1).setPreferredWidth(200)
-                current.columnModel.getColumn(2).setPreferredWidth(120)
+                current.columnModel.getColumn(2).setPreferredWidth(130)
+                current.columnModel.getColumn(3).setMinWidth(0)
                 current.columnModel.getColumn(3).setMaxWidth(0)
+                current.columnModel.getColumn(3).setPreferredWidth(0)
+                current.setFillsViewportHeight(true)
+                current.setPreferredScrollableViewportSize(new Dimension(390,480));
             }
         }
     }
@@ -49,18 +55,18 @@ panel(id: 'wbwPanel', layout: new MigLayout('fillx, wrap 2', '[fill][fill]', '[f
             label()
         }
 
-        panel(background: Color.WHITE, layout: new MigLayout('fill', '[center]', ''), constraints: 'span, grow, height 300px!, width 300px!') {
+        panel(background: Color.WHITE, layout: new MigLayout('fill', '[center]', ''), constraints: 'span, grow, height 250px!, width 250px!') {
             label(id: 'wbwBild', text: '-- kein Bild --', background: Color.WHITE, constraints: 'height 220px!, width 220px!')
         }
 
     }
     // Links unten: Summe aller Einzelwiderstände
-    panel(id: 'wbwSummePanel', layout: new MigLayout('fillx', '[left][right]', '[fill]')) {
+    panel(id: 'wbwSummePanel', layout: new MigLayout('fillx', '[left][right]', '[fill]0[fill]')) {
         label('<html><b>Summe aller Einzelwiderstände</b></html>')
         label(id: 'wbwSumme', text: bind(source: model.meta, sourceProperty: 'summeAktuelleWBW', converter: { v -> "<html><b>${v.toString2()}</b></html>" }))
     }
     // Rechts unten: Buttons
-    panel(id: 'wbwButton', layout: new MigLayout('fillx', '[left][right]', '[fill]')) {
+    panel(id: 'wbwButton', layout: new MigLayout('fillx', '[left][right]', '[fill]0[fill]')) {
         button(id: 'wbwOk', text: 'OK')
         button(id: 'wbwCancel', text: 'Abbrechen')
     }
